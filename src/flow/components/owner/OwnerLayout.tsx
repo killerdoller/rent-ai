33bf19f0c68dfbb85df45c7719f0fe6e7ba5c9df
mafelh111 <mafelopez48@gmail.com>
2026-04-22 14:54:02 -0500

    fix: synchronize owner layout with root for consistent chat behavior

--- src/flow/components/owner/OwnerLayout.tsx
+++ src/flow/components/owner/OwnerLayout.tsx
@@ -95,7 +95,7 @@ export function OwnerLayout({ children }: { children: React.ReactNode }) {
 
       {/* Main Content */}
       <div className="flex flex-col flex-1 min-h-0 md:ml-[240px]">
-        <main className="flex-1 min-h-0 overflow-y-auto flex flex-col">
+        <main className="flex-1 min-h-0 flex flex-col">
           {children}
           {/* Espaciador para la nav móvil fija */}
           <div className="md:hidden" style={{ height: 80, flexShrink: 0 }} />
